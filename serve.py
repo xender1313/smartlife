eb2c7992c9a620a73dd6d47550dc5ce12a004d85
xender1313 <110733835+xender1313@users.noreply.github.com>
2025-11-29 21:00:34 +0100

    Update serve.py

--- serve.py
+++ serve.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
 import sys
-if sys.version_info[0] == 3: # python3
-	from http.server import HTTPServer, CGIHTTPRequestHandler
-else: # python2
-	from BaseHTTPServer import HTTPServer
-	from CGIHTTPServer import CGIHTTPRequestHandler
-import cgitb; cgitb.enable()  ## This line enables CGI error reporting
+
+if sys.version_info[0] == 3:  # python3
+    from http.server import HTTPServer, CGIHTTPRequestHandler
+else:  # python2 (au cas où tu l’utilises encore)
+    from BaseHTTPServer import HTTPServer
+    from CGIHTTPServer import CGIHTTPRequestHandler
+
+# Plus de cgitb ici
 
 if len(sys.argv) > 1:
-	port = int(sys.argv[1])
+    port = int(sys.argv[1])
 else:
-	port = 8000
+    port = 8000
 
 server = HTTPServer
 handler = CGIHTTPRequestHandler
@@ -20,7 +22,13 @@ handler.cgi_directories = ["/cgi-bin"]
 httpd = server(server_address, handler)
 
 if port == 4443:
-	import ssl
-	httpd.socket = ssl.wrap_socket(httpd.socket, keyfile='localhost.pem', certfile="localhost.pem", server_side=True)
+    import ssl
+    httpd.socket = ssl.wrap_socket(
+        httpd.socket,
+        keyfile='localhost.pem',
+        certfile="localhost.pem",
+        server_side=True
+    )
 
 httpd.serve_forever()
+// suprime l'erreur 
